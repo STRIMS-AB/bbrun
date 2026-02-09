ed6bef1307046238cecfbfb84fcaa031ea5652b4
kamaldevj <kelbed@gmail.com>
2026-02-09 22:54:20 +0100

    Remove shadow plugin, use simple fat JAR

--- dist/homebrew/bbrun.rb
+++ dist/homebrew/bbrun.rb
@@ -5,30 +5,26 @@
 class Bbrun < Formula
   desc "HTTP API testing DSL - write expressive API tests"
   homepage "https://github.com/STRIMS-AB/bbrun"
-  version "0.1.3"
+  version "0.1.6"
   license "MIT"
 
   url "https://github.com/STRIMS-AB/bbrun/releases/download/v#{version}/bbrun-#{version}.tar.gz"
-  sha256 "33d361c952ee7e3cce5fd11e230bad98a9644a91a8e0af278c115bb0c2bb40c8"
+  sha256 "PLACEHOLDER_SHA256"
 
   depends_on "openjdk@17"
 
   def install
-    # The tarball extracts to bbrun-0.1.1/ which contains bin/ and lib/
-    # Install everything to libexec, preserving the structure
+    # Install everything to libexec
     libexec.install Dir["*"]
     
-    # Remove Windows batch files
-    rm_f Dir[libexec/"bin/*.bat"]
-    
     # Make the script executable
-    chmod 0755, libexec/"bin/bbrun-cli"
+    chmod 0755, libexec/"bin/bbrun"
     
-    # Create wrapper that sets JAVA_HOME and calls the actual script
+    # Create wrapper that sets JAVA_HOME
     (bin/"bbrun").write <<~EOS
       #!/bin/bash
       export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}"
-      exec "#{libexec}/bin/bbrun-cli" "$@"
+      exec "#{libexec}/bin/bbrun" "$@"
     EOS
     chmod 0755, bin/"bbrun"
   end
